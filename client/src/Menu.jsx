45ecaf4cd13051935d181d70a942cf355d19f9b7
Cody Jue <codytjue@gmail.com>
2021-08-09 16:46:49 -0400

    Continued Daily Goal Bug fixes

--- client/src/Menu.jsx
+++ client/src/Menu.jsx
@@ -157,7 +157,12 @@ const Menu = ({ handleNewMeal, handleDateChange }) => {
   let handleDailyGoalsButton = () => {
     setStatus('goals');
     axios.get('/meals')
-    .then((result) => {console.log("TESTING:", result.data.totals); setTotals(result.data.totals)});
+    .then((result) => {
+      console.log("TESTING:", result.data.totals)
+      if (result.data.totals) {
+        setTotals(result.data.totals);
+      }
+    })
     axios.get('/goals')
     .then((result) => {setGoals(result.data)})
   }
